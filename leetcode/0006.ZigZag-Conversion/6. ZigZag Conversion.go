af34b89fca1a3a11bdc7762c126a1a7a66b7f363
Wenjin Rao <19flipped@gmail.com>
2021-05-27 03:11:01 +0800

    Update 6. ZigZag Conversion.go

--- leetcode/0006.ZigZag-Conversion/6. ZigZag Conversion.go
+++ leetcode/0006.ZigZag-Conversion/6. ZigZag Conversion.go
@@ -1,26 +1,26 @@
 package leetcode
 
 func convert(s string, numRows int) string {
-	matrix, down, up := make([][]byte, numRows, numRows), 0, numRows-2
-	for i := 0; i != len(s); {
-		if down != numRows {
-			matrix[down] = append(matrix[down], byte(s[i]))
-			down++
-			i++
-		} else if up > 0 {
-			matrix[up] = append(matrix[up], byte(s[i]))
-			up--
-			i++
-		} else {
-			up = numRows - 2
-			down = 0
-		}
-	}
-	solution := make([]byte, 0, len(s))
-	for _, row := range matrix {
-		for _, item := range row {
-			solution = append(solution, item)
-		}
-	}
-	return string(solution)
+    if numRows == 1 {
+        return s
+    }
+    r, x, dx := make([][]rune, numRows), 0, 1
+    for _, c := range s {
+        r[x] = append(r[x], c)
+        if dx == 1 {
+            if x == numRows - 1 {
+                dx = -1
+            }
+        } else if dx == -1 {
+            if x == 0 {
+                dx = 1
+            }
+        }
+        x += dx
+    }
+    res := ""
+    for _, row := range r {
+        res += string(row)
+    }
+    return res
 }
