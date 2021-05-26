4cdf8212f2c5a119def81987b2c086a4c754ef61
Wenjin Rao <19flipped@gmail.com>
2021-05-27 00:46:21 +0800

    Update 9. Palindrome Number.go

--- leetcode/0009.Palindrome-Number/9. Palindrome Number.go
+++ leetcode/0009.Palindrome-Number/9. Palindrome Number.go
@@ -4,43 +4,34 @@ import "strconv"
 
 // 解法一
 func isPalindrome(x int) bool {
-	if x < 0 {
-		return false
-	}
-	if x == 0 {
-		return true
-	}
-	if x%10 == 0 {
-		return false
-	}
-	arr := make([]int, 0, 32)
-	for x > 0 {
-		arr = append(arr, x%10)
-		x = x / 10
-	}
-	sz := len(arr)
-	for i, j := 0, sz-1; i <= j; i, j = i+1, j-1 {
-		if arr[i] != arr[j] {
-			return false
-		}
-	}
-	return true
+    if x < 0 {
+        return false
+    }
+    s := make([]int, 0, 9)
+    for x > 0 {
+        s = append(s, x % 10)
+        x /= 10
+    }
+    l := len(s)
+    for i := 0; i < l/2; i++ {
+        if s[i] != s[l-i-1] {
+            return false
+        }
+    }
+    return true
 }
 
 // 解法二 数字转字符串
-func isPalindrome1(x int) bool {
-	if x < 0 {
-		return false
-	}
-	if x < 10 {
-		return true
-	}
-	s := strconv.Itoa(x)
-	length := len(s)
-	for i := 0; i <= length/2; i++ {
-		if s[i] != s[length-1-i] {
-			return false
-		}
-	}
-	return true
+func isPalindrome2(x int) bool {
+    if x < 0 {
+        return false
+    }
+    s := strconv.Itoa(x)
+    l := len(s)
+    for i := 0; i < l/2; i++ {
+        if s[i] != s[l-i-1] {
+            return false
+        }
+    }
+    return true
 }
