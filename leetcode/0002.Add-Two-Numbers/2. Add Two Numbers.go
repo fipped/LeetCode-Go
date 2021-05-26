f06aeda988fecfb16595818b1cbbdb615a7fca75
Wenjin Rao <19flipped@gmail.com>
2021-05-27 01:08:53 +0800

    Update 2. Add Two Numbers.go

--- leetcode/0002.Add-Two-Numbers/2. Add Two Numbers.go
+++ leetcode/0002.Add-Two-Numbers/2. Add Two Numbers.go
@@ -15,6 +15,34 @@ type ListNode = structures.ListNode
  * }
  */
 
+// 1. 把结果存到 l1。
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    head := l1
+    for ; l1 != nil && l2 != nil; l1, l2 = l1.Next, l2.Next {
+        l1.Val += l2.Val
+        addToNext(l1)
+        if l1.Next == nil {
+            l1.Next, l2.Next = l2.Next, l1.Next
+        }
+    }
+    for ; l1 != nil && l1.Val >= 10; l1 = l1.Next {
+        addToNext(l1)
+    }
+    return head
+}
+
+// 进行进位。
+func addToNext(l1 *ListNode) {
+    ten := l1.Val / 10
+    l1.Val %= 10
+    if l1.Next != nil {
+        l1.Next.Val += ten
+    } else if ten > 0 {
+        l1.Next = &ListNode{Val: ten}
+    }
+}
+
+// 2. 新开变量存结果，模拟加法。
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	head := &ListNode{Val: 0}
 	n1, n2, carry, current := 0, 0, 0, head
