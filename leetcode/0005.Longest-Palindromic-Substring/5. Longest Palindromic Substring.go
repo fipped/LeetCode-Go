9e200d7f8fdf3694a91d6c6a012e06ca85bb8335
Wenjin Rao <19flipped@gmail.com>
2021-05-27 02:40:42 +0800

    Update 5. Longest Palindromic Substring.go

--- leetcode/0005.Longest-Palindromic-Substring/5. Longest Palindromic Substring.go
+++ leetcode/0005.Longest-Palindromic-Substring/5. Longest Palindromic Substring.go
@@ -2,45 +2,38 @@ package leetcode
 
 // 解法一 Manacher's algorithm，时间复杂度 O(n)，空间复杂度 O(n)
 func longestPalindrome(s string) string {
-	if len(s) < 2 {
-		return s
-	}
-	newS := make([]rune, 0)
-	newS = append(newS, '#')
-	for _, c := range s {
-		newS = append(newS, c)
-		newS = append(newS, '#')
-	}
-	// dp[i]:    以预处理字符串下标 i 为中心的回文半径(奇数长度时不包括中心)
-	// maxRight: 通过中心扩散的方式能够扩散的最右边的下标
-	// center:   与 maxRight 对应的中心字符的下标
+	// RL[i]:    以预处理字符串下标 i 为中心的回文半径(奇数长度时不包括中心)
+	// p:        通过中心扩散的方式能够扩散的最右边的下标
 	// maxLen:   记录最长回文串的半径
-	// begin:    记录最长回文串在起始串 s 中的起始下标
-	dp, maxRight, center, maxLen, begin := make([]int, len(newS)), 0, 0, 1, 0
-	for i := 0; i < len(newS); i++ {
-		if i < maxRight {
-			// 这一行代码是 Manacher 算法的关键所在
-			dp[i] = min(maxRight-i, dp[2*center-i])
-		}
-		// 中心扩散法更新 dp[i]
-		left, right := i-(1+dp[i]), i+(1+dp[i])
-		for left >= 0 && right < len(newS) && newS[left] == newS[right] {
-			dp[i]++
-			left--
-			right++
-		}
-		// 更新 maxRight，它是遍历过的 i 的 i + dp[i] 的最大者
-		if i+dp[i] > maxRight {
-			maxRight = i + dp[i]
-			center = i
-		}
+	// start:    记录最长回文串在起始串 s 中的起始下标
+    t := make([]rune, len(s) * 2 + 1)
+    RL := make([]int, len(s) * 2 + 1)
+    t[0] = '#'
+    for i, c := range s {
+    	t[2 * i + 1] = c
+        t[2 * i + 2] = '#'
+    }
+    p, start, maxLen := 0, 0, 0
+    for i, _ := range t {
+    	if RL[p] + p > i {
+        	RL[i] = min(RL[2 * p - i], RL[p] + p - i)
+        }
+        // 中心扩散法求 RL[i]
+    	for ; i + RL[i] + 1 < len(t) && i - RL[i] - 1 >= 0 && t[i + RL[i] + 1] == t[i - RL[i] - 1]; RL[i]++ {
+        }
+		// 更新 p，它是遍历过的 i 的 i + RL[i] 的最大者
+        if i + RL[i] > p + RL[p] {
+        	p = i
+        }
 		// 记录最长回文子串的长度和相应它在原始字符串中的起点
-		if dp[i] > maxLen {
-			maxLen = dp[i]
-			begin = (i - maxLen) / 2 // 这里要除以 2 因为有我们插入的辅助字符 #
-		}
-	}
-	return s[begin : begin+maxLen]
+        if RL[i] > maxLen {
+        	maxLen = RL[i]
+            // 偶数长度：#b#a#a# (i=4 即第3个#为中心，maxLen=2，起点为 1）
+            // 奇数长度：#b#a#a#a#（i=5，即第2个a为中心，maxLen=3，起点为 1）
+            start = (i - maxLen) / 2 
+        }
+    }
+    return s[start:start+maxLen]
 }
 
 func min(x, y int) int {
